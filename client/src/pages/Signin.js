f58952a227ca3e8b266ebd6cda418f00abaec0ce
SaadMaybe <88343088+SaadMaybe@users.noreply.github.com>
2022-04-05 03:07:55 +0500

    pushing to originsa
    kmskamd

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,60 +1,136 @@
-import React, { useState } from "react";
-import Axios from "axios";
+import React, { useState, Component } from "react";
+import axios from "axios";
 import "../App.css";
 import { useNavigate } from "react-router";
 
-export default function Signin() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  Axios.defaults.withCredentials = true;
-  let history = useNavigate();
+export default class UserSignIn extends Component {
+  constructor(props) {
+    super(props);
 
-  const login = () => {
-    Axios.post(`/signin`, {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if (response.data.message) {
-        console.log(response.data.message);
-      } else {
-        history.push(`/Homepage`);
-      }
-    });
-    // history(`/Homepage`);
+    this.getNumber = this.getNumber.bind(this);
+    this.getPassword = this.getPassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-  };
+    this.state = {
+      Number: '',
+      Password: ''
+    };
+  }
 
-  return (
-    <div className="App">
-      <div className="login">
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="Username..."
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password..."
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <div>
-          <button onClick={login}> Login </button>
-          <button
-            onClick={() => {
-              history("/Signup");
-            }}
-          >
-            {" "}
-            Signup{" "}
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  getNumber(e) {
+    this.setState({
+      Number: e.target.value
+    })
+  }
+
+  getPassword(e) {
+    this.setState({
+      Password: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      Number: this.state.Number,
+      Password: this.state.Password
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:9000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: '',
+      password: ''
+    })
+  }
+
+  render() {
+    return (
+            <div>
+            <h3>Sign in</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Number: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.Number}
+                    onChange={this.getNumber}
+                    />
+                <br></br>
+                <label>Password: </label>
+                <input  type="password"
+                required
+                className="form-control"
+                value={this.state.Password}
+                onChange={this.getPassword}
+                />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+              </div>
+            </form>
+            </div>
+    )
+  }
 }
+
+// export default function Signin() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   Axios.defaults.withCredentials = true;
+//   let history = useNavigate();
+
+//   const login = () => {
+//     Axios.post(`/signin`, {
+//       username: username,
+//       password: password,
+//     }).then((response) => {
+//       if (response.data.message) {
+//         console.log(response.data.message);
+//       } else {
+//         history.push(`/Homepage`);
+//       }
+//     });
+//     // history(`/Homepage`);
+
+//   };
+//   return (
+//     <div className="App">
+//       <div className="login">
+//         <h1>Login </h1>
+//         <input
+//           type="text"
+//           placeholder="Username..."
+//           onChange={(e) => {
+//             setUsername(e.target.value);
+//           }}
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password..."
+//           onChange={(e) => {
+//             setPassword(e.target.value);
+//           }}
+//         />
+//         <div>
+//           <button onClick={login}> Login </button>
+//           <button
+//             onClick={() => {
+//               history("/Signup");
+//             }}
+//           >
+//             {" "}
+//             Signup{" "}
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
