97b4f5e2e9ba89dbab706065c11da8621f7b64e2
Salman-Masood <93938214+Salman-Masood@users.noreply.github.com>
2022-04-05 20:14:14 +0500

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,112 +1,231 @@
-import React , {useState} from "react";
-import Axios from "axios";
+// import React , {useState} from "react";
+// import Axios from "axios";
+// import "../App.css";
+// import { useNavigate } from "react-router";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+
+// export default function Signup() {
+//   let history = useNavigate();
+//   const [status, setStatus] = useState(false);
+//   const [check, setcheck] = useState(true);
+
+
+//   Axios.defaults.withCredentials = true;
+
+//   const initialValues = {
+//     username: "",
+//     // phone: "",
+//     // name: "",
+//     password: "",
+//   };
+
+//   const validationSchema = Yup.object().shape({
+//     username: Yup.string().min(3).max(15).required(),
+//     password: Yup.string().min(4).max(20).required(),
+//     // phone: Yup.string().required(),
+//     // name: Yup.string().required(),
+//   });
+
+//   const onSubmit = (data) => {
+//     Axios.post("/signup", data).then((response) => {
+//       console.log(data);
+//       if(response.data==="FALSE"){
+//         setcheck(false)
+//       }
+//       else if(response.data){
+//         setcheck(true);
+//         setStatus(false);
+//         history("/");
+//       }
+//       else{
+//         setStatus(true);
+//       }
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <div className="register">
+//         <h1>Signup</h1>
+//         <Formik
+//           initialValues={initialValues}
+//           onSubmit={onSubmit}
+//           validationSchema={validationSchema}
+//         >
+//           <Form className="formContainer">
+//             <Field
+//               autoComplete="off"
+//               id="inputCreatePost"
+//               name="username"
+//               placeholder="Username"
+//             />
+
+//             {/* <Field
+//               autoComplete="off"
+//               type="text"
+//               id="inputCreatePost"
+//               name="name"
+//               placeholder="Name"
+//             />
+
+//             <Field
+//               autoComplete="off"
+//               type="text"
+//               id="inputCreatePost"
+//               name="phone"
+//               placeholder="Contact Number"
+//             /> */}
+
+//             <Field
+//               autoComplete="off"
+//               type="password"
+//               id="inputCreatePost"
+//               name="password"
+//               placeholder="Password"
+//             />
+
+//             <button type="submit"> Signup</button>
+//           {!check ? <div> username already exists</div> : <div></div>}
+
+
+//             <br></br>
+
+//             <ErrorMessage name="password" component="span" />
+//             {status === true && (
+//               <label className="exist">Username Already exist</label>
+//             )}
+//             <br></br>
+//             <ErrorMessage name="username" component="span" />
+//             <br></br>
+//             <ErrorMessage name="name" component="span" />
+//             <br></br>
+//             <ErrorMessage name="phone" component="span" />
+//           </Form>
+//         </Formik>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// //hmm
+
+import React, { useState, Component } from "react";
+import axios from "axios";
 import "../App.css";
 import { useNavigate } from "react-router";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-
-export default function Signup() {
-  let history = useNavigate();
-  const [status, setStatus] = useState(false);
-  const [check, setcheck] = useState(true);
 
 
-  Axios.defaults.withCredentials = true;
+export default class UserSignUp extends Component {
+  constructor(props) {
+    super(props);
 
-  const initialValues = {
-    username: "",
-    // phone: "",
-    // name: "",
-    password: "",
-  };
+    this.getUserName = this.getUserName.bind(this);
+    this.getNumber = this.getNumber.bind(this);
+    this.getEmail = this.getEmail.bind(this);
+    this.getPassword = this.getPassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-  const validationSchema = Yup.object().shape({
-    username: Yup.string().min(3).max(15).required(),
-    password: Yup.string().min(4).max(20).required(),
-    // phone: Yup.string().required(),
-    // name: Yup.string().required(),
-  });
+    this.state = {
+      UserName: '',
+      Number: '',
+      Email: '',
+      Password: ''
+    };
+  }
 
-  const onSubmit = (data) => {
-    Axios.post("/signup", data).then((response) => {
-      console.log(data);
-      if(response.data==="FALSE"){
-        setcheck(false)
-      }
-      else if(response.data){
-        setcheck(true);
-        setStatus(false);
-        history("/");
-      }
-      else{
-        setStatus(true);
-      }
-    });
-  };
+  getUserName(e){
+    this.setState({
+      UserName: e.target.value
+    })
+  }
 
-  return (
-    <div className="App">
-      <div className="register">
-        <h1>Signup</h1>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={onSubmit}
-          validationSchema={validationSchema}
-        >
-          <Form className="formContainer">
-            <Field
-              autoComplete="off"
-              id="inputCreatePost"
-              name="username"
-              placeholder="Username"
-            />
+  getNumber(e) {
+    this.setState({
+      Number: e.target.value
+    })
+  }
 
-            {/* <Field
-              autoComplete="off"
-              type="text"
-              id="inputCreatePost"
-              name="name"
-              placeholder="Name"
-            />
+  getEmail(e){
+    this.setState({
+      Email: e.target.value
+    })
+  }
 
-            <Field
-              autoComplete="off"
-              type="text"
-              id="inputCreatePost"
-              name="phone"
-              placeholder="Contact Number"
-            /> */}
+  getPassword(e) {
+    this.setState({
+      Password: e.target.value
+    })
+  }
 
-            <Field
-              autoComplete="off"
-              type="password"
-              id="inputCreatePost"
-              name="password"
-              placeholder="Password"
-            />
+  onSubmit(e) {
+    e.preventDefault();
 
-            <button type="submit"> Signup</button>
-          {!check ? <div> username already exists</div> : <div></div>}
+    const user = {
+      UserName: this.state.UserName,
+      Number: this.state.Number,
+      Email: this.state.Email,
+      Password: this.state.Password
+    }
 
+    console.log("User: ", user);
 
-            <br></br>
+    axios.post('http://localhost:9000/signup/', user)
+      .then(res => console.log(res.data));
 
-            <ErrorMessage name="password" component="span" />
-            {status === true && (
-              <label className="exist">Username Already exist</label>
-            )}
-            <br></br>
-            <ErrorMessage name="username" component="span" />
-            <br></br>
-            <ErrorMessage name="name" component="span" />
-            <br></br>
-            <ErrorMessage name="phone" component="span" />
-          </Form>
-        </Formik>
-      </div>
-    </div>
-  );
+    this.setState({
+      UserName: '',
+      Number: '',
+      Email: '',
+      Password: ''
+    })
+  }
+
+  render() {
+    return (
+            <div>
+            <h3>Sign Up</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Username: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.UserName}
+                    onChange={this.getUserName}
+                    />
+                <br></br>
+                <label>Number: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.Number}
+                    onChange={this.getNumber}
+                    />
+                <br></br>
+                <label>Email: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.Email}
+                    onChange={this.getEmail}
+                    />
+                <br></br>
+                <label>Password: </label>
+                <input  type="password"
+                required
+                className="form-control"
+                value={this.state.Password}
+                onChange={this.getPassword}
+                />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+              </div>
+            </form>
+            </div>
+    )
+  }
 }
 
-
-//hmm
