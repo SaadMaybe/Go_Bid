a4e15db9676c496043fb3d1c45a1baa316874c23
taimursalman <58456445+taimursalman@users.noreply.github.com>
2022-04-06 07:03:31 +0500

    signup.js updated
    haven't implemented JWTs in here, but other than that, the functionality of the "/signup" route has been implemented
    
    username, phoneNumber, and password are passed to the server

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,5 +1,35 @@
 const router = require('express').Router();
+const Users = require('../models/user.model.js');
+const sha256 = require('sha256');
+router.route("/").post((req, res) => 
+{
+    Users.find().sort({userID: -1}).then(async user => 
+    {
+        var userID;
+        if(user.length == 0)
+            userID = 0;
+        else 
+            userID = user[0].userID + 1;
 
-router.route("/:name").get((req,res) => res.send("hello world, this is: " + req.params['name']));
+        console.log(userID);
+        const username = req.body.username;
+        console.log(username);
+        const password = sha256(req.body.password);
+        const phoneNumber = req.body.phoneNumber;
+        const completedAuctions = [];
+        const completedBids = 0;
+        const cancelledAuctions = 0;
+        const cancelledBids = 0;
+        const account_status = "user";
+
+        const newUsers = new Users({userID ,username, password, phoneNumber, completedAuctions, completedBids, cancelledAuctions, cancelledBids, account_status});
+        await newUsers.save()
+            .then(() => res.json('User added!'))
+            .catch(err => res.status(400).json('Error ' + err));
+        
+    }).catch(err => res.status(400).json('Error: ' + err));
+    
+})
+
 
 module.exports = router;
