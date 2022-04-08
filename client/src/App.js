57b1507239cc6fa9fa41f14a6878ef3ebb814db7
Salman-Masood <93938214+Salman-Masood@users.noreply.github.com>
2022-04-08 15:52:46 +0500

    css updated and contact us page frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 import { SignIn } from './pages/Signin';
 import { Homepage } from './pages/Homepage';
 import { Signup } from './pages/Signup';
+import { ContactUs } from './pages/ContactUs'
 import { useNavigate } from "react-router-dom";
 // import contactUs from './pages/contactUs';
 
@@ -16,7 +17,7 @@ function App() {
           <Route path='/' element={<SignIn/>} />
           <Route path='/Homepage' element={<Homepage/>} />
           <Route path='/Signup' element={<Signup/>} />
-          {/* <Route path='/contactUs' element={<contactUs/>} /> */}
+          <Route path='/ContactUs' element={<ContactUs/>} />
         </Routes>
       </Router>
     </div>
