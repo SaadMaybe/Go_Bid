0926bac9ec8c1c47ccb2156a61dfcc0026de19d9
taimursalman <58456445+taimursalman@users.noreply.github.com>
2022-04-12 22:59:54 +0500

    styling
    styling for view my auctions and view my past auctions
    
    Co-Authored-By: Salman-Masood <93938214+Salman-Masood@users.noreply.github.com>

--- client/src/pages/ViewMyAuctions.jsx
+++ client/src/pages/ViewMyAuctions.jsx
@@ -13,14 +13,21 @@ export const ViewMyAuctions = () =>
     const [auctionList, setAuctionList] = useState([]);
     const [username, setUsername] = useState("");
     const [bidList, setBidList] = useState([]);
-
+    const [auctionID, setAuctionID] = useState(0);
 
     function sellAuction(e)
     {
         console.log("Sell Auction");
-        axios.post("http://localhost:9000/viewMyAuctions/sell", {auctionID: e.target.auctonID}).then(res =>
+        axios.post("http://localhost:9000/viewMyAuctions/sell", {auctionID: auctionID}).then(res =>
         {
+
             console.log("res is " + res.data);
+            if(res.data.status === 'ok')
+                alert("Auction sold!")
+            else
+                alert("Auction not sold!")
+
+            navigate("/ViewMyAuctions", {state: {userID: location.state.userID}});
             
         })
         //Sells an auction to the highest bidder
@@ -29,56 +36,70 @@ export const ViewMyAuctions = () =>
     function cancelAuction(e)
     {
         console.log("Cancel Auction");
-        axios.post("http://localhost:9000/viewMyAuctions/cancel", {auctionID: e.target.auctionId}).then(res =>
+        axios.post("http://localhost:9000/viewMyAuctions/cancel", {auctionID: auctionID}).then(res =>
         {
             console.log("res is " + res.data);
             
+            navigate("/ViewMyAuctions", {state: {userID: location.state.userID}});
         })
         //Cancels an auction
     }
 
 
-    useEffect(() => 
+    useEffect(async() => 
     {
         const hmmm = location.state.userID
         console.log("hmmm is " + hmmm);
-        axios.post('http://localhost:9000/viewMyAuctions', {userID: hmmm}).then(response => 
+        axios.post('http://localhost:9000/viewMyAuctions', {userID: hmmm}).then(async response => 
         {
             if(response.status === 200)
             {
                 setAuctionList(response.data.auctionList);
                 setUsername(response.data.username);
-                setBidList(response.data.bidList);
+                await setBidList(response.data.bidList);
+
             }
             else
             {
                 return "error";
             }
-                        
+
         });
-    }, [location.state.userID]);
+        
+    }, [location.state.userID, location.state.auctionID]);
 
     return (
         <div>
-            <button onClick={() => navigate('/Homepage', {state:{userID: location.state.userID}})}>Go back</button>
+            <div className = "top-dash-user">
+            <div className="back-btn"><button className="back" onClick={() => navigate('/Homepage', {state:{userID: location.state.userID}})}>back;</button> </div>
+            My Auctions
+            </div>
             <br></br>
+            <div className='past-btn'>
+                <button className='past-auction' onClick={() => navigate('/ViewMyPastAuctions', {state:{userID: location.state.userID}})}>Past Auctions</button>
+            </div>
+
             List of auctions for user {username}:
-            <ul>
-                { 
-                auctionList.map((auction, index) =>
-                    <li key={auction.auctionID}>
-                        Title of the auction: {auction.itemBeingAuctioned.itemTitle}
-                        <p>   </p>
-                        Maximum Bid on the auction: {bidList[index]};
-                        <button onClick={() => sellAuction(auction.auctionID)}>Sell</button>
-                        <button onClick={() => cancelAuction(auction.auctionID)}>Cancel</button>                                             
-                        <br></br>
-                        <br></br>
-                    </li>
-                )}
-            </ul>
+            <br></br><br></br><br></br><br></br>
+                <ul>
+                    { 
+                    auctionList.map((auction, index) =>
+                        <div className='playcards'>
+                            <li key={auction.auctionID}>
+                                <div className='in-text'>
+                                    Title of the auction: {auction.itemBeingAuctioned.itemTitle}
+                                    <p>   </p>
+                                    {bidList[index] == auction.itemBeingAuctioned.minimumBid ? "No bids yet. Starting value for the bid is " + auction.itemBeingAuctioned.minimumBid + " " : "Highest bid: " + bidList[index] + " "}
+
+                                    <button className='sell' onClick={() => {setAuctionID(auction.auctionID); sellAuction();}}>Sell</button>
+                                    <button className='cancel' onClick={() => {setAuctionID(auction.auctionID); cancelAuction();}}>Cancel</button> 
+                                </div>
+
+                            </li>
+                        </div>
+                    )}
+                </ul>
             <div>
-                <button onClick={() => navigate('/ViewMyPastAuctions', {state:{userID: location.state.userID}})}>Past Auctions</button>
             </div>
         </div>
     )
