06a2ceabf738582558aaa0341cf0ffd4a4915e7c
SaadMaybe <88343088+SaadMaybe@users.noreply.github.com>
2022-04-14 00:58:19 +0500

    admin portal
    there are some problems with banning etc

---
+++ client/src/pages/AdminDisplayUsers.jsx
@@ -0,0 +1,90 @@
+import React, { useState, Component, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
+
+
+export const AdminDisplayUsers = () => {
+    
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [userID, setUserID] = useState(0);
+    const [username, setUsername] = useState('');
+    const [userInfo, setUserInfo] = useState([]);
+    const [un, setUn] = useState(0);
+    
+    function BanUser(e)
+    {
+        var unlucky = un;
+        axios.post('http://localhost:9000/adminPortal/banUser', {userID: unlucky}).then(res =>
+        {
+            if(res.status === 200)
+            {
+                alert("User: " + res.data.username + " has been banned");
+                navigate("/AdminPortal", {state: {userID: userID}});
+            }
+            else
+            {
+                alert("There was an error in banning the user");
+                navigate("/AdminPortal", {state: {userID: userID}});
+            }
+        });
+    }
+
+    useEffect(async () => {
+        const hmm = location.state.userID;
+        
+        await setUserID(hmm);
+
+        const h = location.state.username;
+        await setUsername(h);
+
+        const huh = location.state.info;
+        
+        await setUserInfo(huh);
+        
+    }, [location.state.userID, location.state.info]);
+
+    
+
+    return (
+        <div>
+            <div className = "top-dash-user">
+            <div className="back-btn"><button className="back" onClick={() => navigate('/AdminPortal', {state:{userID: location.state.userID}})}>&#8249;</button></div>
+            Admin Portal
+            </div>
+            
+            <div className="portal">
+                <br></br>
+                Admin Name: {username}
+                <br></br>
+                <br></br>
+                List of users:
+                <ul>
+                    { 
+                    userInfo.map(user =>
+                        <div className="playcards">
+                            <li key={user.userID}>
+                                <div className="in-text">
+                                    User: {user.username}<br></br>
+                                    Phone Number: {user.phoneNumber}<br></br>
+                                    Email: {user.email}
+                                    <p>   </p>
+                                    
+                                    <button className="cancel" onClick={() => {setUn(user.userID);BanUser()}}>Ban User</button>
+                                    <br></br>
+                                    <br></br>
+                                </div>
+                            </li>
+                        </div>
+                    )}
+                </ul>
+            </div>
+        </div>
+        
+    )
+
+    
+}
---
+++ client/src/pages/AdminPortal.jsx
@@ -0,0 +1,212 @@
+import React, { useState, Component, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
+
+
+
+
+export const AdminPortal = () => 
+{
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [userID, setUserID] = useState(0);
+    const [username, setUsername] = useState('');
+    const [inpUsername, setInpUsername] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [itemName, setItemName] = useState('');
+    const [auctioner, setAuctioner] = useState('');
+
+
+    // function handleClick(e) {
+    //     e.preventDefault();
+    //     navigate(-1, {state:{userID: location.state.userID}});
+    // }
+
+    function changeInpUsername(v)
+    {
+        setInpUsername(v.target.value);
+    }
+    function changePhoneNumber(v)
+    {
+        setPhoneNumber(v.target.value);
+    }
+    function changeItemName(v)
+    {
+        setItemName(v.target.value);
+    }
+    function changeAuctioner(v)
+    {
+        setAuctioner(v.target.value);
+    }
+
+    function userOnSubmit(e)
+    {
+        e.preventDefault();
+        
+        
+        if(inpUsername !== "" && phoneNumber !== "")
+        {
+            
+            axios.post('http://localhost:9000/adminPortal/searchPhoneNumber', {username: inpUsername, phoneNumber: phoneNumber}).then(res =>
+            {
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/User', {state:{username: username, userID: userID, info: res.data.userArr}});
+                else
+                {
+                    alert("No user found with that username and phone number");
+                    navigate('/AdminPortal', {state:{userID: userID}});
+                    
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else if(inpUsername !== '')
+        {
+            axios.post('http://localhost:9000/adminPortal/searchUsername', {username: inpUsername}).then(res =>
+            {
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/User', {state:{username: username, userID: userID, info: res.data.userArr}});
+                else
+                {
+                    alert("No user found with that username and phone number");
+                    navigate('/AdminPortal', {state:{userID: userID}});
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else if(phoneNumber !== '')
+        {
+            axios.post('http://localhost:9000/adminPortal/searchPhoneNumber', {phoneNumber: phoneNumber}).then(res =>
+            {
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/User', {state:{username: username, userID: userID, info: res.data.userArr}});
+                else
+                {
+                    alert("No user found with that username and phone number");
+                    navigate('/AdminPortal', {state:{userID: userID}});
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else
+        {
+            return <div>bruh</div>;
+        }
+        
+        
+        
+    }
+
+    function auctionOnSubmit(e)
+    {
+        e.preventDefault();
+        if(itemName !== '' && auctioner !== '')
+        {
+            axios.post('http://localhost:9000/adminPortal/searchAuctioner', {auctioner: auctioner}).then(res =>
+            {
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/Auction', {state:{username: username, userID: userID, info: res.data.auctions}});
+                else
+                {
+                    alert("No auctions found");
+                    navigate('/AdminPortal', {state:{username: username, userID: userID}});
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else if(itemName !== '')
+        {
+            
+            axios.post('http://localhost:9000/adminPortal/searchItemName', {itemName: itemName}).then(res =>
+            {
+                
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/Auction', {state:{username: username, userID: userID, info: res.data.auctions}});
+                else
+                {
+                    alert("No auctions found");
+                    navigate('/AdminPortal', {state:{username: username, userID: userID}});
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else if(auctioner !== '')
+        {
+            axios.post('http://localhost:9000/adminPortal/searchAuctioner', {auctioner: auctioner}).then(res =>
+            {
+                if(res.data.status === 'ok')
+                    navigate('/AdminPortal/Auction', {state:{username: username, userID: userID, info: res.data.auctions}});
+                else
+                {
+                    alert("No auctions found");
+                    navigate('/AdminPortal', {state:{username: username, userID: userID}});
+                }
+                //something
+            }).catch(err => {return <div>{err}</div>});
+        }
+        else
+        {
+            return <div>bruh</div>;
+        }
+
+    }
+
+
+    useEffect(async () => {
+        
+        const hmmm = location.state.userID;
+        setUserID(hmmm);
+
+        axios.post('http://localhost:9000/adminPortal/', {userID: hmmm}).then(res => 
+        {
+            if(res.data.status === 'ok')
+                setUsername(res.data.username);   
+            else
+            {
+                alert("Error: User not found");
+                navigate("/Homepage", {state: {userID: hmmm}});
+            }    
+
+        });
+    }, [])
+
+
+    return (
+        <div>
+            <div className = "top-dash-user">
+            <div className="back-btn"><button className="back" onClick={() => navigate('/Homepage', {state:{userID: location.state.userID}})}>&#8249;</button></div>
+            Admin Portal
+            </div>
+            
+                <br></br><br></br>
+                <div className="portal">
+                    Admin Portal for {username}:<br></br>
+                    <br></br>
+                    
+                    <form className="by-user">
+                        <h2>Search by User</h2>
+                        <label>Search by Username</label>
+                        <input className = "in-user" type="text" onChange={changeInpUsername} />
+                        <label>Search by phone Number</label>
+                        <input className = "in-user" type="text" onChange={changePhoneNumber} />
+                        <button className="search-portal" onClick={userOnSubmit}>Search</button>
+                    </form>
+                    <br></br><br></br>
+                    <form className="by-user">
+                        <h2>Search by Auction</h2>
+                        <label>Search by Item Name</label>
+                        <input className="in-user" type="text" onChange={changeItemName} />
+                        <label>Search by Auctioner</label>
+                        <input className = "in-user" type="text" onChange={changeAuctioner} />
+                        <button className="search-portal" onClick={auctionOnSubmit}>Search</button>
+                    </form>
+
+                </div>
+
+            
+        </div>
+    )
+}
