b6e15fb98f63630532a47f7329a6413568589d0c
ahmedtaimoor <23100281@lums.edu.pk>
2022-04-14 01:43:36 +0500

    post a bid

--- server/routes/postabid.js
+++ server/routes/postabid.js
@@ -4,9 +4,9 @@ let UsersModel = require('../models/user.model');
 let ItemModel = require('../models/item.model');
 let BidModel = require('../models/bid.model');
 
-router.route('/').post((req,res) => 
+router.route('/').post(async (req,res) => 
 {
-    BidModel.find().sort({bidID:-1}).then(async bid =>
+    await BidModel.find().sort({bidID:-1}).then(async bid =>
     {
         var bidID;
 
@@ -30,17 +30,16 @@ router.route('/').post((req,res) =>
         .then(() => res.json('Bid added!'))
         .catch(err => res.status(400).json('Error ' + err));
 
-        let list = AuctionModel.findOne({_id : associatedAuction}).listofBids;
+        let list = await AuctionModel.findOne({_id : associatedAuction}).listofBids;
 
-        const temp = BidModel.findOne({bidID : bidID});
+        const temp = await BidModel.findOne({bidID : bidID});
 
         list = list.push(temp._id);
 
         console.log(list);
         
-        console.log(AuctionModel.findOne({_id: associatedAuction }));
         
-        const updateAuction = AuctionModel.updateOne({_id: associatedAuction} , {$set : {listofBids : list}});
+        const updateAuction = await AuctionModel.updateOne({_id: associatedAuction} , {$set : {listofBids : list}});
 
         await updateAuction.save()
         .then(() => res.json('Auction updated!'))
