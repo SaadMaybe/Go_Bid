0ddafc23204b7bfd43f65354e18cbdb8c122c6d3
Salman-Masood <93938214+Salman-Masood@users.noreply.github.com>
2022-04-17 22:11:15 +0500

    homepage revamped

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -72,18 +72,51 @@ export const Homepage = () => {
     return (
         <html>
         <head>
-            <title>W3.CSS Template</title>
-            <meta charset="UTF-8" />
+                <meta charset="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
             
         </head>
     
         <body>
         <div>
-            {/* <h3>Homepage</h3> */}
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item mrg">
+                        <a class="nav-link nav_button" onClick={() => navigate('/ViewMyBids', {state: {userID : userID, id: id}})}>My Bids</a>
+                    </li>
+                    <li class="nav-item mrg">
+                        <a class="nav-link nav_button" onClick={() => navigate('/ContactUs', {state: {userID : userID, id: id}})}>Contact us</a>
+                    </li>
+                    <li class="nav-item mrg">
+                        <a class="nav-link nav_button" onClick={() => navigate('/ViewMyAuctions', {state: {userID : userID, id: id}})}>My Auctions</a>
+                    </li>
+                    <li class="nav-item mrg">
+                        <a class="nav-link nav_button" onClick={() => navigate('/PostAnAuction', {state: {userID : userID, id: id}})}> Post an auction</a>
+                    </li>
+                    <li class="nav-item mrg">
+                        <a class="nav-link nav_button" onClick={() => 
+                                    {
+                                        setUserID(0);
+                                        navigate('/');
+                                    }}> 
+                                    Logout</a>
+                    </li>
+                </ul>
+                <a class="nav-link nav_button" onClick={() => myNav()}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg> 
+                            Hello there  {username}!</a>
+
+            </div>
+        </nav>
+
+
+
+
+            {/* <h3>Homepage</h3>
             <div className='big-black'>
                 <div className='inner'>
                     <ul>
@@ -135,28 +168,29 @@ export const Homepage = () => {
                     
                     </ul>
                 </div>
-            </div>
+            </div> */}
             <br></br>
-            <div className='Search-Bar'>
-                <form onSubmit={onsubmitSearch}>
-                <label>Search:</label>
+            <div className='Search-Bar search_css'>
+                <form className='fo-m' onSubmit={onsubmitSearch}>
+                <label className='s_bid'>GoBid</label>
                 <input  type="text"
                     required
-                    className="form-control"
+                    className="form-control w-50 s_input" placeholder='Search'
                     // value={this.state.UserName}
                     onChange={changeSearchQuery}
                     />
-                <button type='submit'>Search</button>
+                <button className='s_button' type='submit'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                                            </svg>
+                </button>
                 </form>
             </div>
-            <br></br>
-            <br></br>
-
-            <div className='down-one'> 
+                        
+        <div className='down-one'> 
             <div className='popular'>Popular Items</div>
             {
             auctions.map((auction, index) => 
-            <div className='row'>
+            <div className='row1 .col-md-2'>
                 <div className='item'>
                 <img src="https://cdn.shopify.com/s/files/1/0161/0482/products/ayegear_tshirt_5_pockets_multipocket_travel_scottevest_navy.jpg?v=1538484272"></img>
                         <div className='text-desc'>
@@ -175,7 +209,7 @@ export const Homepage = () => {
                             {/* <h12> */}
                                 <b>Highest Bid: {bidList[index]}</b>
                             {/* </h12> */}
-                            <button onClick={() => navigate('/DisplayAnAuction', {state: {userID: userID, auctionid: auction._id, maximumBid: bidList[index], id: id, highestBid: auction.highestBid, highestBidValue: auction.highestBidValue}})}>View Auction</button>
+                            <button className='btn btn-myauction' onClick={() => navigate('/DisplayAnAuction', {state: {userID: userID, auctionid: auction._id, maximumBid: bidList[index], id: id, highestBid: auction.highestBid, highestBidValue: auction.highestBidValue}})}>View Auction</button>
                         </div>
 
                 </div>
