fd2b71b380ea219ff19075780b4a42445d89fd94
taimursalman <58456445+taimursalman@users.noreply.github.com>
2022-04-17 22:36:34 +0500

    search results bug fixed
    and picture replaced with location

--- client/src/pages/SearchResults.jsx
+++ client/src/pages/SearchResults.jsx
@@ -115,7 +115,7 @@ export const SearchResults = () => {
                             {/* <h12> */}
                                 <b>Highest Bid: {bidList[index]}</b>
                             {/* </h12> */}
-                            <button onClick={() => navigate('/DisplayAnAuction', {state: {userID: userID, auctionid: auction._id, maximumBid: bidList[index], id: id}})}>View Auction</button>
+                            <button onClick={() => navigate('/DisplayAnAuction', {state: {userID: userID, auctionid: auction._id, highestBidValue: bidList[index], id: id}})}>View Auction</button>
                         </div>
 
                 </div>
