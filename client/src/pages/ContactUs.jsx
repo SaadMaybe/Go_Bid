18db1d090e0f06ec03040b53cc3632a679419bbe
Salman-Masood <93938214+Salman-Masood@users.noreply.github.com>
2022-04-18 02:30:11 +0500

    styling done

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -51,55 +51,46 @@ export const ContactUs = () => {
     }
   
   }
-//   return (
-//                 <div className="left">
-//                 <form onSubmit={onSubmit}>
-//                   <div className="form-group">
-//                     <h3>Contact Us</h3> 
-//                     <label>Email: </label>
-//                     <input  type="text"
-//                         required
-//                         className="form-control"
-//                         onChange={changeEmail}
-//                         />
-//                     <br></br>
-//                     <label>Subject: </label>
-//                     <input  type="password"
-//                     required
-//                     className="form-control"
-//                     onChange={changeSubject}
-//                     />
-//                     <br></br>
-//                     <label>How can we help you?: </label>
-//                     <input  type="password"
-//                     required
-//                     className="form-control"
-//                     onChange={changeMsg}
-//                     />
-//                     <input type="submit" value="Submit" className="btn btn-primary" />
-//                     <div className="right"></div>
-//                   </div>
-//                 </form>
-//                 </div>
-//         )
-// }
-  return (
-    <div>
-    	<div className = "top-dash-user">
-    		<div className="back-btn">
-			<button 
-			className="back" 
-			onClick=
-				{
-					() => {navigate('/Homepage', {state:{userID: location.state.userID, id: location.state.id}})}
-				}>&#8249;</button>
-			</div>
-			Contact Us
-			</div>
-      
-			Feel free to contact us at GoBid@gmail.com		
-      </div>
 
+  return (
+    // <div>
+    // 	<div className = "top-dash-user">
+    // 		<div className="back-btn">
+		// 	<button 
+		// 	className="back" 
+		// 	onClick=
+		// 		{
+		// 			() => {navigate('/Homepage', {state:{userID: location.state.userID, id: location.state.id}})}
+		// 		}>&#8249;</button>
+		// 	</div>
+		// 	Contact Us
+		// 	</div>
+      
+		// 	Feel free to contact us at GoBid@gmail.com		
+    //   </div>
+    <div className="user-outer">
+    <div className = "top-dash-user">
+    <div className="back-btn"><button className="back" onClick={() => navigate(-1, {state:{userID: location.state.userID}})}>&#8249;</button> </div>
+    Contact Us
+    </div>
+
+    <div className="profile-bg">
+        
+    </div>
+
+    <div className="profile-details">
+        {/* <button onClick={() => navigate(-1, {state:{userID: location.state.userID}})}>Go back</button> */}
+        <br></br><br></br>
+    
+        <div className="user-details">
+            Email: gobid@hotmail.com
+            <br></br><br></br>
+            Phone No.: 090078601
+           
+        </div>
+        {/* <div className="hojana">{(accountStatus==="admin") ? <button className="btn btn-primary" onClick={() => navigate('/AdminPortal', {state:{userID: location.state.userID}})}>Admin Portal</button> : null}</div>     */}
+    </div>
+    </div>
 		
         )
 
